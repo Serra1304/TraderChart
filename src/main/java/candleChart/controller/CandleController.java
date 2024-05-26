f2a8288cce837c595ee8df50aa7cf0f5a4f3a083
Serra1304 <serrajgt87@gmail.com>
2024-05-26 17:16:49 +0200

    Verificacion de datos, documentacion y pruebas unitarias de CandleController

--- src/main/java/candleChart/controller/CandleController.java
+++ src/main/java/candleChart/controller/CandleController.java
@@ -6,40 +6,55 @@ import candleChart.view.CandleView;
 
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Controller for candle view (CandleView) on the candle chart.
  */
 public class CandleController {
+    private static final double DEFAULT_MAX_PRICE = -Double.MAX_VALUE;
+    private static final double DEFAULT_MIN_PRICE = Double.MAX_VALUE;
 
     private final CandleView candleView;
+    private List<Candle> candleList;
+    private CandleSize candleSize;
+    private int currentCandleIndex; // Indice de inicio de la lista de velas.
 
     private Buffer buffer;
-    private List<Candle> candleList;
-
     private double maxPrice, minPrice;
 
-    private CandleSize candleSize;
-    private int candlePosition;
-
+    /**
+     * Constructor de la clase CandleController.
+     * Crea una nueva instancia de la clase CandleView con los valores predeterminados.
+     *
+     * @param candleView Instancia de la vista para este controlador.
+     */
     public CandleController(CandleView candleView) {
         this.candleView = candleView;
 
-        candlePosition = 0;
+        candleList = new ArrayList<>();
         candleSize = CandleSize.SMALL;
+        currentCandleIndex = 0;
         buffer = new Buffer();
         setupView();
     }
+
 
     /**
      * Establece un buffer de datos en el controlador.
      *
      * @param buffer El buffer de datos a establecer en el controlador.
+     * @throws NullPointerException Si el buffer proporcionado es nulo.
      */
     public void setBuffer(Buffer buffer) {
+        if(buffer == null) {
+            throw new NullPointerException("No se permiten valores nulos para el valor 'buffer'.");
+        }
         this.buffer = buffer;
+        updateCandleView();
     }
+
 
     /**
      * Obtiene el buffer de datos del controlador.
@@ -50,6 +65,7 @@ public class CandleController {
         return buffer;
     }
 
+
     /**
      * Obtiene el precio máximo que será representado en el gráfico.
      *
@@ -58,6 +74,7 @@ public class CandleController {
     public double getMaxPrice() {
         return maxPrice;
     }
+
 
     /**
      * Obtiene el precio mínimo que será representado en el gráfico.
@@ -68,78 +85,146 @@ public class CandleController {
         return minPrice;
     }
 
+
+    /**
+     * Establece un objeto de tipo 'CandleSize' que proporciona información sobre el tamaño de las velas.
+     *
+     * @param candleSize Un objeto de tipo 'CandleSize'.
+     * @throws NullPointerException Si el valor proporcionado de 'candleSize' es nulo.
+     */
     public void setCandleSize(CandleSize candleSize) {
+        if(candleSize == null) {
+            throw new NullPointerException("No se permiten valores nulos para 'candleSize'.");
+        }
         this.candleSize = candleSize;
         candleView.setCandleSize(candleSize);
         updateCandleView();
     }
 
+
+    /**
+     * Obtiene un objeto de tipo 'CandleSize' que proporciona información del tamaño de la vela.
+     *
+     * @return Objeto de tipo 'CandleSize' con información del tamaño de la vela.
+     */
     public CandleSize getCandleSize() {
         return candleSize;
     }
 
+
+    /**
+     * Avanza un paso en la representación del gráfico siempre que no haya llegado al principio de este.
+     */
     public void advance() {
-        candlePosition--;
-        updateCandleView();
+        advance(1);
     }
 
+
+    /**
+     * Avanza en el gráfico el número de pasos proporcionado por parámetros. Si el número pasado por parámetro
+     * sobrepasa el inicio del buffer, se establece el inicio del buffer como primer valor a representar.
+     *
+     * @param steps Número de pasos a avanzar en el buffer.
+     * @throws IllegalArgumentException Si el parámetro proporcionado es negativo.
+     */
     public void advance(int steps) {
-        candlePosition -= steps;
+        if(steps < 0) {
+            throw new IllegalArgumentException("No se permiten valores negativos.");
+        }
+
+        currentCandleIndex = Math.max(currentCandleIndex - steps, 0);
         updateCandleView();
     }
 
+
+    /**
+     * Retrocede un paso en la representación del gráfico siempre que no se haya llegado al final del buffer.
+     */
     public void retrieve() {
-        candlePosition++;
-        updateCandleView();
+        retrieve(1);
     }
 
+
+    /**
+     * Retrocede el número de velas proporcionado por parámetro. Si el número de pasos proporcionados para retroceder
+     * supera a los que puede ser representado por el buffer, se establece el máximo permitido para la correcta
+     * representación de la fracción del buffer
+     *
+     * @param steps El número de pasos a retroceder en el buffer.
+     * @throws IllegalArgumentException Si el parámetro proporcionado es negativo.
+     */
     public void retrieve(int steps) {
-        candlePosition += steps;
+        if(steps < 0) {
+            throw new IllegalArgumentException("No se permiten valores negativos.");
+        }
+
+        int maxSteps = buffer.size() - candleList.size() - 1;
+        currentCandleIndex = maxSteps - currentCandleIndex > steps? currentCandleIndex + steps: maxSteps ;
         updateCandleView();
     }
 
+
+    /**
+     * Configura los distintos elementos de la vista.
+     */
     private void setupView() {
         candleView.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
                 updateCandleView();
-
-                candleView.setCandleSize(candleSize);
             }
         });
     }
 
+
+    /**
+     * Método encargado de llamar a todos los demás métodos auxiliares para la actualización de la vista.
+     */
     private void updateCandleView() {
         updateCandleList();
         updatePriceRange();
-
     }
 
-    private void updateCandleList() {
-        int numVelas = Math.max(candleView.getWidth() / candleSize.getRelativePosition(), 0);
-        int candleLast = buffer.size() -1 - candlePosition;
-        int candleFirst = Math.max(candleLast - numVelas, 0);
 
-        List<Candle> candleList = buffer.getAll().subList(candleFirst, candleLast);
-        this.candleList = candleList;
+    /**
+     * Método que establece la lista de velas que deben ser representadas en la vista.
+     */
+    private void updateCandleList() {
+        int visibleCandleCount = Math.max(candleView.getWidth() / candleSize.getRelativePosition(), 0);
+        calculateVisibleCandleRange(visibleCandleCount);
         candleView.setCandleList(candleList);
     }
 
-    private void updatePriceRange() {
-        maxPrice = -999999999;
-        minPrice = 999999999;
 
-        for (Candle candle : candleList) {
-            if (candle.highPrice() > maxPrice) {
-                maxPrice = candle.highPrice();
-            }
-            if (candle.lowPrice() < minPrice) {
-                minPrice = candle.lowPrice();
-            }
+    /**
+     * Método auxiliar que establece el rango de velas a mostrar en la vista.
+     * @param visibleCandleCount Número de velas que pueden ser representadas en la vista.
+     */
+    private void calculateVisibleCandleRange(int visibleCandleCount) {
+        int bufferSize = buffer.size();
+        if (bufferSize == 0) {
+            currentCandleIndex = 0;
+            return;
         }
+        currentCandleIndex = Math.min(currentCandleIndex, bufferSize - 1); // Índice de inicio de las velas a mostrar.
+        int candleLast = bufferSize - 1 - currentCandleIndex;   // Índice de la ultima vela a mostrar (Índice inferior).
+        int candleFirst = Math.max(candleLast - visibleCandleCount, 0); // Índice de la primera vela (Índice superior).
+        candleList = buffer.getAll().subList(candleFirst, candleLast);
+    }
+
+
+    /**
+     * Método auxiliar que establece el rango de precio en la vista (candleView). En caso de existir una lista de velas
+     * vacía, se establece el rando de precio mínimo y máximo en 0. En caso contrario se establecerá el rango superior
+     * con el mayor valor de precio de la lista y el rango inferior con el rango inferior de la lista.
+     */
+    private void updatePriceRange() {
         if (candleList.isEmpty()) {
             maxPrice = 0;
             minPrice = 0;
+        } else {
+            maxPrice = candleList.stream().mapToDouble(Candle::highPrice).max().orElse(DEFAULT_MAX_PRICE);
+            minPrice = candleList.stream().mapToDouble(Candle::lowPrice).min().orElse(DEFAULT_MIN_PRICE);
         }
         candleView.setPriceRange(maxPrice, minPrice);
     }
--- src/test/java/candleChart/controller/CandleControllerTest.java
+++ src/test/java/candleChart/controller/CandleControllerTest.java
@@ -1,87 +1,166 @@
-//package candleChart.controller;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import java.time.LocalDateTime;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import candleChart.model.Candle;
-//import candleChart.view.CandleView;
-//
-//public class CandleControllerTest {
-//    private CandleController candleController;
-//    private CandleView candleView;
-//    private ChartController chartController;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        candleView = Mockito.mock(CandleView.class);
-//        chartController = Mockito.mock(ChartController.class);
-//        candleController = new CandleController(candleView);
-//    }
-//
-//    @Test
-//    public void testUpCandleSize() {
-//        candleController.setCandleWidth(5);
-//
-//        assertTrue(candleController.upCandleSize());
-//        assertEquals(7, candleController.getCandleWidth());
-//        assertEquals(11, candleController.getRelativePosition());
-//    }
-//    @Test
-//    public void testUpCandleSize_Limit() {
-//        candleController.setCandleWidth(9);
-//
-//        assertFalse(candleController.upCandleSize());
-//        assertEquals(9, candleController.getCandleWidth());
-//        assertEquals(13, candleController.getRelativePosition());
-//    }
-//
-//    @Test
-//    public void testDownCandleSize() {
-//        candleController.setCandleWidth(7);
-//
-//        assertTrue(candleController.downCandleSize());
-//        assertEquals(5, candleController.getCandleWidth());
-//        assertEquals(9, candleController.getRelativePosition());
-//    }
-//
-//    @Test
-//    public void testDownCandleSize_Limit() {
-//        candleController.setCandleWidth(3);
-//
-//        assertFalse(candleController.downCandleSize());
-//        assertEquals(3, candleController.getCandleWidth());
-//        assertEquals(7, candleController.getRelativePosition());
-//    }
-//
-//    @Test
-//    public void testSetCandle_WidthInRange() {
-//        assertTrue(candleController.setCandleWidth(5));
-//        assertEquals(5, candleController.getCandleWidth());
-//
-//        assertTrue(candleController.setCandleWidth(6));
-//        assertEquals(7, candleController.getCandleWidth());
-//    }
-//
-//    @Test
-//    public void testSetCandle_WidthOutRange() {
-//        assertFalse(candleController.setCandleWidth(2));
-//        assertFalse(candleController.setCandleWidth(10));
-//    }
-//
-//    @Test
-//    public void testSetCandleBuffer() {
-//        List<Candle> candleBuffer = new ArrayList<>();
-//        candleBuffer.add(new Candle(LocalDateTime.of(2023, 10, 2, 0, 0), 10, 20, 5, 15));
-//        candleBuffer.add(new Candle(LocalDateTime.of(2023, 10, 2, 1, 0), 15, 25, 12, 18));
-//
-//        candleController.setCandleBuffer(candleBuffer);
-//        assertEquals(candleBuffer, candleController.getCandleBuffer());
-//    }
-//}
-//
+package candleChart.controller;
+
+import candleChart.data.Buffer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import candleChart.model.Candle;
+import candleChart.view.CandleView;
+
+public class CandleControllerTest {
+    private CandleController candleController;
+    private CandleView candleView;
+
+    private Buffer buffer;
+
+    @BeforeEach
+    public void setUp() {
+        ArrayList<Candle> candleList = new ArrayList<>();
+        candleList.add(new Candle(LocalDateTime.now(), 1.25, 1.30, 1.20, 1.22));
+        candleList.add(new Candle(LocalDateTime.now(), 2.25, 2.30, 2.20, 2.22));
+        candleList.add(new Candle(LocalDateTime.now(), 1.15, 1.20, 1.10, 1.12));
+        candleList.add(new Candle(LocalDateTime.now(), 2.25, 2.30, 2.20, 2.22));
+        candleList.add(new Candle(LocalDateTime.now(), 1.25, 1.30, 1.20, 1.22));
+        candleList.add(new Candle(LocalDateTime.now(), 2.25, 2.30, 2.20, 2.22));
+        candleList.add(new Candle(LocalDateTime.now(), 1.15, 1.20, 1.10, 1.12));
+        candleList.add(new Candle(LocalDateTime.now(), 2.25, 2.30, 2.20, 2.22));
+
+        candleView = Mockito.mock(CandleView.class);
+        candleController = new CandleController(candleView);
+
+        buffer = spy(new Buffer());
+        buffer.addAll(candleList);
+    }
+
+    @Test
+    public void testSetBuffer_NullBuffer() {
+        NullPointerException exception;
+        exception = assertThrows(NullPointerException.class, () -> {
+            candleController.setBuffer(null);
+        });
+        assertEquals("No se permiten valores nulos para el valor 'buffer'.", exception.getMessage());
+    }
+
+    @Test
+    public void testSetBuffer_ValidBuffer() {
+        candleController.setBuffer(buffer);
+
+        assertEquals(buffer, candleController.getBuffer());
+    }
+
+    @Test
+    public void testGetMaxPrice() {
+        when(candleView.getWidth()).thenReturn(200);
+
+        candleController.setBuffer(buffer);
+        double maxPrice = buffer.getAll().stream().mapToDouble(Candle::highPrice).max().orElse(0);
+
+        assertEquals(maxPrice, candleController.getMaxPrice());
+    }
+
+    @Test
+    public void testGetMinPrice() {
+        when(candleView.getWidth()).thenReturn(200);
+
+        candleController.setBuffer(buffer);
+        double minPrice = buffer.getAll().stream().mapToDouble(Candle::lowPrice).min().orElse(0);
+
+        assertEquals(minPrice, candleController.getMinPrice());
+    }
+
+    @Test
+    public void testSetCandleSize_NullCandleSize() {
+        NullPointerException exception;
+        exception = assertThrows(NullPointerException.class, () -> {
+            candleController.setCandleSize(null);
+        });
+        assertEquals("No se permiten valores nulos para 'candleSize'.", exception.getMessage());
+    }
+
+    @Test
+    public void testSetCandleSize_ValidCandleSize() {
+        CandleSize candleSize = CandleSize.VERY_LARGE;
+        candleController.setCandleSize(candleSize);
+
+        assertEquals(candleSize, candleController.getCandleSize());
+    }
+
+    @Test
+    public void testAdvance_OneStep() {
+        candleController = spy(new CandleController(new CandleView()));
+        candleController.advance();
+
+        verify(candleController, times(1)).advance(1);
+    }
+
+    @Test
+    public void testAdvance_NegativeStep() {
+        IllegalArgumentException exception;
+        exception = assertThrows(IllegalArgumentException.class, () -> {
+            candleController.advance(-2);
+        });
+        assertEquals("No se permiten valores negativos.", exception.getMessage());
+    }
+
+    @Test
+    public void testAdvance_ValidStep() {
+        when(candleView.getWidth()).thenReturn(10);
+        candleController.setBuffer(buffer);
+        candleController.retrieve(4);
+        candleController.advance(2);
+
+        verify(candleView, atLeastOnce()).setCandleList(buffer.getAll().subList(4,5));
+    }
+
+    @Test
+    public void testAdvance_OverStep() {
+        when(candleView.getWidth()).thenReturn(10);
+        candleController.setBuffer(buffer);
+        candleController.retrieve(4);
+        candleController.advance(20);
+
+        verify(candleView, atLeastOnce()).setCandleList(buffer.getAll().subList(6,7));
+    }
+
+    @Test
+    public void testRetrieve_OneStep() {
+        candleController = spy(new CandleController(new CandleView()));
+        candleController.retrieve();
+
+        verify(candleController, times(1)).retrieve(1);
+    }
+
+    @Test
+    public void testRetrieve_NegativeStep() {
+        IllegalArgumentException exception;
+        exception = assertThrows(IllegalArgumentException.class, () -> {
+            candleController.retrieve(-2);
+        });
+        assertEquals("No se permiten valores negativos.", exception.getMessage());
+    }
+
+    @Test
+    public void testRetrieve_ValidStep() {
+        when(candleView.getWidth()).thenReturn(10);
+        candleController.setBuffer(buffer);
+        candleController.retrieve(4);
+
+        verify(candleView, atLeastOnce()).setCandleList(buffer.getAll().subList(2,3));
+    }
+
+    @Test
+    public void testRetrieve_OverStep() {
+        when(candleView.getWidth()).thenReturn(10);
+        candleController.setBuffer(buffer);
+        candleController.retrieve(20);
+
+        verify(candleView, atLeastOnce()).setCandleList(buffer.getAll().subList(0,1));
+    }
+}
