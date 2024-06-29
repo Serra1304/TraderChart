c076716eb921fedb6a49d8ede7aa6feea7a57861
Serra1304 <serrajgt87@gmail.com>
2024-06-29 20:37:50 +0200

    implementacion del sistema modular

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module candleChart {
+    requires java.desktop;
+
+    exports candleChart;
+    exports candleChart.controller;
+    exports candleChart.data;
+    exports candleChart.view;
+    exports candleChart.model;
+    exports candleChart.exceptions;
+}
